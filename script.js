a3f6faf39500261fce6c61f271f45d2c3a94b1b1
Bria Samuels <bnsamuels@crimson.ua.edu>
2016-10-22 19:57:49 -0400

    Fixed grid code. Adjusted container dimensions.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
 
   for(var x = 0; x < 16; x++){
+    $('#container').append("<div class = 'row'></div>");//16 rows in container
+
     for(var y = 0; y < 16; y++){
-      var block = $("<div class='block'></div>");
-      block.appendTo('#container');
+      $('#container').append("<div class = 'square'></div>");//16 squares, per row
     }
   }
+
+
 });
